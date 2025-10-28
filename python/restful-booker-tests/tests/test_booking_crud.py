dd1f2d148abbd47f90ac46672e47f9f6f7783177
richard hill <rbhill3@gmail.com>
2025-10-28 13:34:22 -0400

    add test_update_booking_with_invalid_id

--- python/restful-booker-tests/tests/test_booking_crud.py
+++ python/restful-booker-tests/tests/test_booking_crud.py
@@ -40,11 +40,24 @@ def test_update_booking_with_invalid_token(booking_api):
     data = create_resp.json()
     booking_id = data["bookingid"]
 
-    # 3. Try to update booking with invalid token
+    # 2. Try to update booking with invalid token
     token = "0"
     updated_payload = {**payload, "lastname": "Updated"}
     update_resp = booking_api.update(booking_id, updated_payload, token)
     assert update_resp.status_code == 403
+
+
+def test_update_booking_with_invalid_id(booking_api, auth_api):
+    # 1. Create a booking
+    payload = sample_booking("Alice", "Smith")
+    create_resp = booking_api.create(payload)
+    assert create_resp.status_code == 200
+
+    # 2. Try to update booking with invalid bookingId
+    token = auth_api.create_token()
+    updated_payload = {**payload, "lastname": "Updated"}
+    update_resp = booking_api.update(99999, updated_payload, token)
+    assert update_resp.status_code == 405
 
 
 def test_delete_booking_with_invalid_token(booking_api):
@@ -56,7 +69,7 @@ def test_delete_booking_with_invalid_token(booking_api):
     data = create_resp.json()
     booking_id = data["bookingid"]
 
-    # 3. Try to delete booking with invalid token
+    # 2. Try to delete booking with invalid token
     token = "0"
     delete_resp = booking_api.delete(booking_id, token)
     assert delete_resp.status_code == 403
